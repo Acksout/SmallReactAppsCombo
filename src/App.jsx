a34ef73baff278adf27ddeff9e07c3d16b17b8fd
Rakshit Bhardwaj <157364563+Acksout@users.noreply.github.com>
2024-09-30 13:43:30 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Accordion from "./listofapps/Accordion";
+
+const App = () => {
+  return (
+    <div>
+      <Accordion />
+    </div>
+  );
+};
+
+export default App;
